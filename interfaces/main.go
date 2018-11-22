4c70863873b91445eec6a304930e2a048b046488
crojas <c.rojas.latorre@gmail.com>
2018-11-22 16:50:47 -0300

    interfaces

---
+++ interfaces/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+type englishBot struct{}
+type spanishBot struct{}
+type bot interface {
+	getGreeting() string
+}
+
+func (eb englishBot) getGreeting() string {
+	return "Hi There!"
+}
+
+func (eb spanishBot) getGreeting() string {
+	return "Hola!"
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+
+}
