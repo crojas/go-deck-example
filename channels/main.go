6bfbba4d98d5f3a0c8a8ca780229db38cdadac27
crojas <c.rojas.latorre@gmail.com>
2018-11-22 22:56:06 -0300

    update

---
+++ channels/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	// loop forever, se ejecuta cuando el canal responde y genera una nueva rutina.
+	// l es el link que retorna el canal.
+	// todo esta dentro de una funcion literal
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println("The website", link, "is down")
+		c <- link
+		return
+	}
+	fmt.Println("The website", link, "is up")
+	c <- link
+}
